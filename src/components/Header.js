4fa5fc39a4cf6df692d142240a3b0fc9681a5739
Yuriy Koziy <yukoziy@gmail.com>
2022-06-09 15:25:14 -0400

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import '../style/header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="Logo">
+        <h1>Shopping Cart!</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
