6e70867a0707ed3e47578f3fdc4ef010ae446824
Yuriy Koziy <yukoziy@gmail.com>
2022-06-18 17:06:08 -0400

    rendering sample data

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import products from '../data/products';
+
 const Shop = () => {
-  return <h1>Hello from shop.</h1>;
+  const [shopData, setShopData] = useState([]);
+
+  useEffect(() => {
+    setShopData(products);
+  }, []);
+
+  const dataToListItem = (data) => {
+    return (
+      <li key={data.id}>
+        Name: {data.name}
+        <br />
+        Description: {data.description}
+        <br />
+        Price: {data.price}
+        <br />
+        Image: {data.image}
+      </li>
+    );
+  };
+
+  const renderProducts = () => {
+    let listItems = [];
+    for (let i = 0; i < shopData.length; i++) {
+      listItems.push(dataToListItem(shopData[i]));
+    }
+    console.log(listItems);
+    return listItems;
+  };
+
+  return (
+    <div>
+      <h1>Hello from shop.</h1>
+      <ul>{renderProducts()}</ul>
+    </div>
+  );
 };
 
 export default Shop;
---
+++ src/data/products.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: 'example=product-1',
+    name: 'Example Product 1',
+    description: 'An amazing product that everyone must buy.',
+    price: 45.99,
+    image: 'some image 1',
+  },
+  {
+    id: 'example=product-2',
+    name: 'Example Product 2',
+    description: 'An amazing product that everyone must buy, right now.',
+    price: 29.99,
+    image: 'some image 2',
+  },
+];
+
+export default products;
