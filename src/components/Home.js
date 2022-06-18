0ad3d6a048d1fb29cf1780b5a599135f80da979e
Yuriy Koziy <yukoziy@gmail.com>
2022-06-18 16:27:54 -0400

    Shop Now button now navigates to shop page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import '../style/home.scss';
 import landingBg from '../assets/landing_bg_test.jpg';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleShopNowClick = (e) => {
+    navigate('/shop');
+  };
+
   return (
     <main>
       <article className="Section1">
@@ -9,7 +16,7 @@ const Home = () => {
         <div className="CallToAction">
           <h1>Shop at my store!!!</h1>
           <h4>Shop random items that you dont need.</h4>
-          <button>Shop Now</button>
+          <button onClick={handleShopNowClick}>Shop Now</button>
         </div>
       </article>
     </main>
