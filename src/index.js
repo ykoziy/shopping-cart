ddf09d9ff6390587d2ad420cd7302ac756b7fe95
Yuriy Koziy <yukoziy@gmail.com>
2023-02-25 19:38:39 -0500

    fix react routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Router } from 'react-router-dom';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
